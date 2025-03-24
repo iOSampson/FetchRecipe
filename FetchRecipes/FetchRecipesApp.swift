b7d12fe59efe48d55e09d8078d058fe332462370
Sampson Ezieme <63993951+iOSampson@users.noreply.github.com>
2025-03-24 13:43:46 -0400

    first commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Sampson Ezieme on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
---
+++ FetchRecipes/RecipeListView.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeListView.swift
+//  FetchRecipes
+//
+//  Created by Sampson Ezieme on 2/26/25.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
